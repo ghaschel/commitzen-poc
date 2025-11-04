4b547ff33dc6bd92da8c2ed11d8fd7a14b179a5e
Guilherme Haschel <ghaschel@gmail.com>
2025-11-04 17:12:42 -0300

    feat: ftesdasdas

--- scripts/pre-commit.mjs
+++ scripts/pre-commit.mjs
@@ -1,8 +1,16 @@
 import { execSync } from "child_process";
 
+import versionrc from "../.versionrc.js";
+
+const { infile } = versionrc;
+
+const changelogPath = path.resolve(infile);
+
+const changelogsDir = path.resolve("changelogs");
+
 // --- Stage files and amend the release commit ---
 try {
-  execSync(`git add .`);
+  execSync(`git add ${changelogsDir} ${changelogPath}`);
   console.log(`✅ Staged split changelog files for release commit.`);
 
   // Amend the commit to include the changelog files without changing the message
