7f00548be625ee58799181cc223f41df67c394d3
Guilherme Haschel <ghaschel@gmail.com>
2025-11-04 17:03:50 -0300

    feat: test
    test

--- .versionrc.js
+++ .versionrc.js
@@ -4,7 +4,7 @@ module.exports = {
   header: "# Changelog\n\n",
   releaseCommitMessageFormat: "chore(release): v{{currentTag}}",
   scripts: {
-    postchangelog: "node scripts/split-changelog.mjs",
+    postcommit: "node scripts/split-changelog.mjs",
     posttag: "node scripts/custom-tag-message.mjs",
   },
   issuePrefixes: ["INSE-"],
